9209b81ec073dd1ac414460c8e30263c7097ceab
Abbey <abbey_sellers1@baylor.edu>
2022-09-22 11:21:17 -0500

    updates2

--- Coin Toss.py
+++ Coin Toss.py
@@ -1,4 +1,6 @@
 import CoinClass as c
+#c is the ailias for CoinClass
+#when importing, it has to be in the same folder to just call it COinClass, if it is not you have to do the whole name thing. 
 
 
 # The main function.
@@ -7,12 +9,14 @@ def main():
        my_coin = c.Coin()   # this creates an instance called 'my_coin' of the class 'Coin()'
 
        # Display the side of the coin that is facing up.
-       print('This side is up:', my_coin.get_sideup())    # notice you do not have to supply the argument/parameter
+       print('This side is up:', my_coin.get_sideup())    # notice you do not have to supply the argument/parameter, it pulls sideup from the coinclass file
+
 
        # Toss the coin.
        print('I am going to toss the coin ten times:')
        for count in range(10):
            my_coin.toss()
+           my_coin.__sideup='Heads'
            
            # Display the side of the coin that is facing up.
            print('This side is up:', my_coin.get_sideup())
